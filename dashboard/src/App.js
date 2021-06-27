5ffc6c96efc4bbdbeb33644dd7a0969a2a93606f
Yang Fei <yang.fei@educationhorizons.com>
2021-06-27 15:00:22 +1000

    add chart

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Table from './components/Table'
+import Table from './components/Table';
+import Chart from './components/Chart';
 
 function App() {
   return (
     <>
       <h1>Dashboard</h1>
       <Table />
+      <Chart />
     </>
   );
 }
